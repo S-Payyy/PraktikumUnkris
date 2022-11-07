6c00b146a98ed88d01f76fec234035ce3a3d1779
xcl.Osie <49903431+xcl404@users.noreply.github.com>
2022-11-07 20:03:31 +0700

    New

---
+++ Praktikum2022/Pertemuan 1/latihan2.py
@@ -0,0 +1,12 @@
+import time
+start_time = time.time()
+def pertama():
+    print("hello")
+    print("world")
+    print("hello world")
+
+    print("hello cantiek")
+    #comment
+    a=10
+    print(a)
+    print(time.time() -start_time, "detik")
